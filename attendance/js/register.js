9f1f98b26cbfd83e39401912f7b4086c3d7a3bbb
Bonace <ombatibonace@gmail.com>
2026-05-12 22:36:41 +0300

    Stopping autoReload in register.html form

--- attendance/js/register.js
+++ attendance/js/register.js
@@ -1,23 +1,24 @@
 let btnCreate = document.getElementById('btnCreate')
 
-btnCreate.addEventListener('click', () => {
+btnCreate.addEventListener('click', (e) => {
+    e.preventDefault();
 let txtfname = document.getElementById('txtfname').value
 let txtlname = document.getElementById('txtlname').value
 let txtemail = document.getElementById('txtemail').value
 let txtpass = document.getElementById('txtpass').value
 let txtconpass = document.getElementById('txtconpass').value
 
-if(txtfname == "" || txtemail == ""){
-    alert("Name and Email must be filled")
+if(txtfname == "" || txtemail == "" || txtpass == "" ){
+    alert("Name, Email and Password must be filled")
 }else{
-    if(txtconpass == txtpass){
+    if(txtconpass == txtpass) {
         let emailid = txtemail.replace(/\./g, "_dot_").replace(/@/g, "_at_")
-        let status = "active"
+        let status = "inactive"
         let timenow = Date.now();
         firebase.auth().createUserWithEmailAndPassword(txtemail, txtpass)
         .then((userCredential) =>{
-            firebase.databaseURL().ref('userDetails/' + emailid).set({
-                FirstNmae: txtfname,
+            firebase.database().ref('userDetails/' + emailid).set({
+                FirstName: txtfname,
                 LastName: txtlname,
                 Email: txtemail,
                 Status: status,
@@ -28,10 +29,11 @@ if(txtfname == "" || txtemail == ""){
         }) 
         .catch((error) => {
             console.log(error)
-            alert("Error creating account")
+            alert(error.message)
         })
     }else{
         alert("Password do not match")
     }
 }
+
 })
