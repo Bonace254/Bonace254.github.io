6fd1b67b2030c3bcf5c6a8cffc338f7b944add4a
Bonace <ombatibonace@gmail.com>
2026-05-13 10:38:04 +0300

    log in

--- attendance/js/login.js
+++ attendance/js/login.js
@@ -17,6 +17,26 @@ btnlogin.addEventListener('click', () => {
         .then((snapshot) => {
             const userDetails = snapshot.val()
             const role = userDetails.role
+            const status = userDetails.Status
+            if(status == "active"){
+                if(role == "Admin"){
+                    //admin dashboard
+                    window.location.href = "dashboard.html"
+                }else if(role == "Student"){
+                    //student dashboard
+                    alert("Student logged in!")
+                }else{
+                    //active users with no roles
+                    alert("No role added yet, contact admin!")
+                }
+            }else{
+                //inactive accounts
+                alert("Account not active, contact admin!")
+            }
+        })
+        .catch((error) =>{
+            alert("Wrong Credentials")
+            btnlogin.innerHTML = "Log in"
         })
     }
 })
